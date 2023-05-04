f9fbabacad89577117d9c6a7222a42476feeb17e
KarimPozzato <94352240+KarimPozzato@users.noreply.github.com>
2023-05-04 19:28:49 +0200

    Add files via upload

---
+++ AddressGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_AddressGenerator
+{
+    interface IAddress
+    {
+        string generateIPv4();
+        string generateSubnet();
+    }
+
+    internal class AddressGenerator : IAddress
+    {
+        private uint bits; //int, la variabile uint inizia a contenere da 0
+
+        public AddressGenerator(uint bits)
+        {
+            this.bits = bits;
+        }
+
+        public string generateIPv4()
+        {
+            byte[] bytes = BitConverter.GetBytes(bits);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes); // se il sistema è little-endian, invertiamo l'ordine dei byte
+            }
+
+            return new IPAddress(bytes).ToString();
+        }
+
+        public string generateSubnet()
+        {
+            byte[] subnetMask = BitConverter.GetBytes(~(bits - 1));
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(subnetMask);
+            }
+
+            return new IPAddress(subnetMask).ToString();
+        }
+    }
+}
+
+/*interface IAddress
+{
+    string generateIPv4();
+    string generateSubnet();
+}
+
+class AddressGenerator : IAddress
+{
+    private uint ipAddress;
+
+    public AddressGenerator(uint ipAddress)
+    {
+        this.ipAddress = ipAddress;
+    }
+
+    public string generateIPv4()
+    {
+        return new IPAddress(ipAddress).ToString();
+    }
+
+    public string generateSubnet()
+    {
+        return "255.255.255.0";
+    }
+}*/
